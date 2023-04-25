56fafc7438b74df01befc123df7e04548f635ee6
Ali <sabirali2560@gmail.com>
2023-04-25 16:45:07 -0500

    Change username for mysql

--- change_table.sh
+++ change_table.sh
@@ -5,15 +5,15 @@ database_name="database_name"
 engine_name="InnoDB"
 
 # Change the storage engine for the database
-mysql -u username -p password -e "ALTER DATABASE ${database_name} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-mysql -u username -p password -e "USE ${database_name};"
+mysql -u root -p password -e "ALTER DATABASE ${database_name} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -u root -p password -e "USE ${database_name};"
 
 # Generate a list of SQL statements to change the storage engine for each table
-tables=$(mysql -u username -p password -Nse "SHOW TABLES" ${database_name})
+tables=$(mysql -u root -p password -Nse "SHOW TABLES" ${database_name})
 for table in $tables; do
   sql_statement="ALTER TABLE ${table} ENGINE=${engine_name};"
   echo "Changing engine for table ${table}..."
-  mysql -u username -p password -e "${sql_statement}" ${database_name}
+  mysql -u root -p password -e "${sql_statement}" ${database_name}
 done
 
 echo "Done."
