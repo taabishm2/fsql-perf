5b05ce4a5fe07f6c6272d936270fda4ec1d9e069
Ali <sabirali2560@gmail.com>
2023-04-25 16:56:51 -0500

    add quotes

--- change_table.sh
+++ change_table.sh
@@ -11,7 +11,7 @@ mysql -u root -p password -e "USE ${database_name};"
 # Generate a list of SQL statements to change the storage engine for each table
 tables=$(mysql -u root -p password -Nse "SHOW TABLES" ${database_name})
 for table in $tables; do
-  sql_statement="ALTER TABLE ${table} ENGINE=${engine_name};"
+  sql_statement="ALTER TABLE ${table} ENGINE='${engine_name}';"
   echo "Changing engine for table ${table}..."
   mysql -u root -p password -e "${sql_statement}" ${database_name}
 done
