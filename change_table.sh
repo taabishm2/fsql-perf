775f9fc670902a14033cd573499b9d5ea1480f20
Ali <sabirali2560@gmail.com>
2023-04-25 16:43:29 -0500

    Modify change table script

--- change_table.sh
+++ change_table.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-MYSQL_USER="root"
-MYSQL_PASSWORD="password"
-MYSQL_HOST="localhost"
-MYSQL_DATABASE="database_name"
+# Set the database name and new storage engine
+database_name="database_name"
+engine_name="InnoDB"
 
-# Get a list of tables in the database
-TABLES=$(mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} ${MYSQL_DATABASE} -e "SHOW TABLES;" | awk '{print $1}' | grep -v '^Tables')
+# Change the storage engine for the database
+mysql -u username -p password -e "ALTER DATABASE ${database_name} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -u username -p password -e "USE ${database_name};"
 
-# Set the default storage engine for the database
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} ${MYSQL_DATABASE} -e "ALTER DATABASE ${MYSQL_DATABASE} DEFAULT STORAGE ENGINE = InnoDB;"
-
-# Change the storage engine for each table
-for TABLE in ${TABLES}; do
-    mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} ${MYSQL_DATABASE} -e "ALTER TABLE ${TABLE} ENGINE = InnoDB;"
+# Generate a list of SQL statements to change the storage engine for each table
+tables=$(mysql -u username -p password -Nse "SHOW TABLES" ${database_name})
+for table in $tables; do
+  sql_statement="ALTER TABLE ${table} ENGINE=${engine_name};"
+  echo "Changing engine for table ${table}..."
+  mysql -u username -p password -e "${sql_statement}" ${database_name}
 done
+
+echo "Done."
